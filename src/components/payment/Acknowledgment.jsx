f161ec149eb1c925acc146abf40f8bee0f6642d2
HeinrichKamil <hansolo12344321@gmail.com>
2025-12-14 18:50:25 +0100

    acknowledgment for payment page

---
+++ src/components/payment/Acknowledgment.jsx
@@ -0,0 +1,15 @@
+const Acknowledgment = () => {
+    return (
+        <div className="auth-form" id="thanks">
+            <h2>Dziękujemy za płatność</h2>
+            <p>
+                Twoje zamówienie zostało opłacone i jest w trakcie realizacji.
+            </p>
+            <p>
+                Warzywa już się boją.
+            </p>
+        </div>
+    );
+};
+
+export default Acknowledgment;
